d5c2c76f9f2b4bf6b4e65eaca64836d694cd1205
Marko <Marko@DESKTOP-FFVEM1G>
2019-11-05 01:08:06 +0100

    Done for local

--- test-app/src/main/java/main/App.java
+++ test-app/src/main/java/main/App.java
@@ -12,6 +12,7 @@ import java.util.stream.Stream;
 import models.DirectoryLocalImplementation;
 import models.DirectoryRemoteManipulation;
 import models.FileLocalImplementation;
+import models.FileRemoteManipulation;
 import users.User;
 
 public class App {
@@ -43,6 +44,8 @@ public class App {
 		String username = sc.nextLine();
 		System.out.print("Enter password: ");
 		String password = sc.nextLine();
+		// <----- DO OVDE JE SVE ISTO ----->
+		
 		// init local storage ( 1. za postojace 2. za novo)
 		if (storageType.equals("local")) {
 			DirectoryLocalImplementation localSt = new DirectoryLocalImplementation();
@@ -125,24 +128,130 @@ public class App {
 			
 			// hc test start
 			boolean[] tmp1 = { (false), (false), (false), (true) };
-			user.createUser("korisnik1", "pswrd", tmp1, localSt.getRoot());
-			user.createUser("korisnik2", "12345", tmp1, localSt.getRoot());
-			user.listAllUsers(localSt.getRoot());
+			user.createUser("korisnik1", "pswrd", tmp1, localSt.getRoot(), "");
+			user.createUser("korisnik2", "12345", tmp1, localSt.getRoot(), "");
+			user.listAllUsers(localSt.getRoot(), "");
 			
 			localSt.createDirectory("novi folder", localSt.getRoot(), user);
 			// hc test end
 			
+			// komunikacija preko komandne linije
 			String command = "";
-			while (!command.equals("exit")) {
-				// TODO ifovi za sve komande
+			while (true) {
 				System.out.print("Command: ");
 				command = sc.nextLine();
+				if (command.equals("exit")) break;
+				// ako nema spejs a nije lsusr onda je greska
+				if (!command.contains(" ") && !command.equals("lsusr")) {
+					System.out.println("Wrong input!");
+					continue;
+				}
+				if (command.equals("lsusr")) {
+					// ovo je jedina komanda bez spejsa
+					user.listAllUsers(localSt.getRoot(), "");
+					continue;
+				}
+				String prefix = command.substring(0, command.indexOf(" "));
+				//System.out.println(prefix);
+				switch(prefix) {
+					case "mkf":
+						// mkf file_name destination_path
+						// Create File (name, dest_path, user)
+						String[] mkfparts = command.split(" ");
+						localF.createFile(mkfparts[1], localSt.getRoot() + mkfparts[2], user);
+						break;
+					case "rmf":
+						// rm file_path
+						// Delete File (file_path, user)
+						String rmfpath = command.substring(command.indexOf(" ") + 1);
+						localF.deleteFile(localSt.getRoot() + rmfpath, user);
+						break;
+					case "upf":
+						// upf file_path dest_path
+						// Upload File (file_path, dest_path, user)
+						String[] upfparts = command.split(" ");
+						localF.uploadFile(upfparts[1], localSt.getRoot() + upfparts[2], user);
+						break;
+					case "dlf":
+						// dlf file_path dest_path
+						// Download File (file_path, dest_path, user)
+						String dlfparts[] = command.split(" ");
+						localF.downloadFile(localSt.getRoot() + dlfparts[1], dlfparts[2], user);
+						break;
+					case "mkdir":
+						// mkdir name dest_path
+						// Create Directory (name, dest_path, user)
+						String[] mkdirparts = command.split(" ");
+						localSt.createDirectory(mkdirparts[1], localSt.getRoot() + mkdirparts[2], user);
+						break;
+					case "rmdir":
+						// rmdir path
+						// Delete Directory (path, user)
+						String rmdirpath = command.substring(command.indexOf(" ") + 1);
+						localSt.deleteDirectory(localSt.getRoot() + rmdirpath, user);
+						break;
+					case "updir":
+						// updir dir_path dest_path
+						// Upload Directory (dir_path, dest_path, user)
+						String[] updirparts = command.split(" ");
+						localSt.uploadDirectory(updirparts[1], localSt.getRoot() + updirparts[2], user);
+						break;
+					case "dldir":
+						// dldir dir_path dest_path
+						// Download Directory (dir_path, dest_path, user)
+						String[] dldirparts = command.split(" ");
+						localSt.downloadDirectory(localSt.getRoot() + dldirparts[1], dldirparts[2], user);
+						break;
+					case "ls-a":
+						// ls-a path
+						// List All Files (path)
+						String lsapath = command.substring(command.indexOf(" ") + 1);
+						localSt.listAllFiles(localSt.getRoot() + lsapath);
+						break;
+					case "ls":
+						// lf path
+						// List Files (path, "all")
+						String lspath = command.substring(command.indexOf(" ") + 1);
+						localSt.listFiles(localSt.getRoot() + lspath, "all");
+						break;
+					case "ls-s":
+						// lf path extension
+						// List Files With Given Extension (path, extension)
+						String lssparts[] = command.split(" ");
+						localSt.listFiles(localSt.getRoot() + lssparts[1], lssparts[2]);
+						break;
+					case "ls-d":
+						// ls-d path
+						// List Directories (path)
+						String lsdpath = command.substring(command.indexOf(" ") + 1);
+						localSt.listDirectories(localSt.getRoot() + lsdpath);
+						break;
+					case "mkusr":
+						// mkusr username password priv1 priv2 priv3 priv4
+						// Create User (username, password, privs[], root, "")
+						String[] parts = command.split(" ");
+						boolean[] privs = {parts[3].equals("true")?true:false, parts[4].equals("true")?true:false, parts[5].equals("true")?true:false, parts[6].equals("true")?true:false};
+						user.createUser(parts[1], parts[2], privs, localSt.getRoot(), "");
+						break;
+					case "rmusr":
+						// rmusr username
+						// Delete User (username, root, "")
+						String usrnm = command.substring(command.indexOf(" ") + 1);
+						user.deleteUser(usrnm, localSt.getRoot(), "");
+						break;
+					default:
+						System.out.println("Wrong input! Command does not exist...");
+				}
+				
 			}
 			System.out.println("Exiting...");
-			
+			sc.close();
 		} else {
-			// TODO remote initStorage
+			// REMOTE ODAVDE
+			// TODO initRemoteStorage
 			DirectoryRemoteManipulation remoteSt = new DirectoryRemoteManipulation();
+			//FileRemoteManipulation remoteF = new FileRemoteManipulation();
+			User user = null;
 			System.out.println("radi remote........");
 		}
 	}

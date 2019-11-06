10320cdeadc174653052d437a98f87db88cc3768
Marko <Marko@DESKTOP-FFVEM1G>
2019-11-06 01:56:29 +0100

    Added missing commands and help

--- test-app/src/main/java/main/App.java
+++ test-app/src/main/java/main/App.java
@@ -140,12 +140,11 @@ public class App {
 			}
 
 			// hc test start
-			boolean[] tmp1 = { (false), (false), (false), (true) };
-			user.createUser("korisnik1", "pswrd", tmp1, localSt.getRoot(), "");
-			user.createUser("korisnik2", "12345", tmp1, localSt.getRoot(), "");
-			user.listAllUsers(localSt.getRoot(), "");
-
-			localSt.createDirectory("novi folder", localSt.getRoot(), user);
+			//boolean[] tmp1 = { (false), (false), (false), (true) };
+			//user.createUser("korisnik1", "pswrd", tmp1, localSt.getRoot(), "");
+			//user.createUser("korisnik2", "12345", tmp1, localSt.getRoot(), "");
+			//user.listAllUsers(localSt.getRoot(), "");
+			//localSt.createDirectory("novi folder", localSt.getRoot(), user);
 			// hc test end
 
 			// komunikacija preko komandne linije
@@ -155,6 +154,37 @@ public class App {
 				command = sc.nextLine();
 				if (command.equals("exit"))
 					break;
+				if (command.equals("help")) {
+					System.out.println("1 - General Commands:");
+					System.out.println("exit     -     [Exit the program.]");
+					System.out.println("help     -     [Displays all commands.]");
+					
+					System.out.println("2 - User Operations:");
+					System.out.println("mkusr username password priv1 priv2 priv3 priv4     -     [Create User]");
+					System.out.println("rmusr username     -     [Delete User]");
+					System.out.println("lsusr     -     [List All Users]");
+					
+					System.out.println("3 - File Operations:");
+					System.out.println("mkf file_name destination_path     -     [Create File]");
+					System.out.println("rmf file_name     -     [Delete File]");
+					System.out.println("upf file_path destination_path     -     [Upload File]");
+					System.out.println("upmf file1_path file2_path... destination_path archive_name     -     [Upload Multiple Files]");
+					System.out.println("dlf file_path destination_path     -     [Download File]");
+					
+					System.out.println("4 - Directory Operations:");
+					System.out.println("mkdir dir_name destination_path     -     [Create Directory]");
+					System.out.println("mkdir {number} dir_name destination_path     -     [Create {number} Directories");
+					System.out.println("rmdir dir_path     -     [Delete Directory]");
+					System.out.println("updir dir_path destination_path     -     [Upload Directory]");
+					System.out.println("updir-z dir_path destination_path     -     [Upload Zipped Directory");
+					System.out.println("dldir dir_path destination_path     -     [Download Directory]");
+					System.out.println("ls-a dir_path     -     [List All Files]");
+					System.out.println("ls dir_path     -     [List Files]");
+					System.out.println("ls-s dir_path extension     -     [List Files With Given Extension]");
+					System.out.println("ls-d dir_path     -     [List Directories]");
+					
+					continue;
+				}
 				// ako nema spejs a nije lsusr onda je greska
 				if (!command.contains(" ") && !command.equals("lsusr")) {
 					System.out.println("Wrong input!");
@@ -175,7 +205,7 @@ public class App {
 					localF.createFile(mkfparts[1], localSt.getRoot() + mkfparts[2], user);
 					break;
 				case "rmf":
-					// rm file_path
+					// rmf file_path
 					// Delete File (file_path, user)
 					String rmfpath = command.substring(command.indexOf(" ") + 1);
 					localF.deleteFile(localSt.getRoot() + rmfpath, user);
@@ -186,6 +216,20 @@ public class App {
 					String[] upfparts = command.split(" ");
 					localF.uploadFile(upfparts[1], localSt.getRoot() + upfparts[2], user);
 					break;
+				case "upmf":
+					// upmf file1_path file2_path... destination_path archive_name
+					// Upload Multiple Files
+					String[] mfparts = command.split(" ");
+					String[] multipleFiles = new String[mfparts.length - 3];
+					int j = 0;
+					for (int i = 1 ; i < mfparts.length - 2 ; i++) {
+						multipleFiles[j] = mfparts[i];
+						j++;
+					}
+					//System.out.println(localSt.getRoot() + mfparts[mfparts.length-2]);
+					//System.out.println(mfparts[mfparts.length-1]);
+					localF.uploadMultipleFilesZip(multipleFiles, localSt.getRoot() + mfparts[mfparts.length-2], mfparts[mfparts.length-1], user);
+					break;
 				case "dlf":
 					// dlf file_path dest_path
 					// Download File (file_path, dest_path, user)
@@ -194,9 +238,19 @@ public class App {
 					break;
 				case "mkdir":
 					// mkdir name dest_path
+					// mkdir {number} name dest_path
 					// Create Directory (name, dest_path, user)
 					String[] mkdirparts = command.split(" ");
-					localSt.createDirectory(mkdirparts[1], localSt.getRoot() + mkdirparts[2], user);
+					if (mkdirparts.length == 3) {
+						localSt.createDirectory(mkdirparts[1], localSt.getRoot() + mkdirparts[2], user);
+					} else {
+						String numb = mkdirparts[1].substring(1, mkdirparts[1].length() - 1);
+						//System.out.println(numb);
+						int n = Integer.parseInt(numb);
+						for (int i = 1 ; i <= n ; i++) {
+							localSt.createDirectory(mkdirparts[2] + i, localSt.getRoot() + mkdirparts[3], user);
+						}
+					}
 					break;
 				case "rmdir":
 					// rmdir path
@@ -209,6 +263,12 @@ public class App {
 					// Upload Directory (dir_path, dest_path, user)
 					String[] updirparts = command.split(" ");
 					localSt.uploadDirectory(updirparts[1], localSt.getRoot() + updirparts[2], user);
+					break;
+				case "updir-z":
+					// updir-z dir_path dest_path
+					// Upload Zipped Directory (dir_path, dest_path, user)
+					String[] updirzparts = command.split(" ");
+					localSt.uploadZipDirectory(updirzparts[1], localSt.getRoot() + updirzparts[2], user);
 					break;
 				case "dldir":
 					// dldir dir_path dest_path
@@ -223,13 +283,13 @@ public class App {
 					localSt.listAllFiles(localSt.getRoot() + lsapath);
 					break;
 				case "ls":
-					// lf path
+					// ls path
 					// List Files (path, "all")
 					String lspath = command.substring(command.indexOf(" ") + 1);
 					localSt.listFiles(localSt.getRoot() + lspath, "all");
 					break;
 				case "ls-s":
-					// lf path extension
+					// ls path extension
 					// List Files With Given Extension (path, extension)
 					String lssparts[] = command.split(" ");
 					localSt.listFiles(localSt.getRoot() + lssparts[1], lssparts[2]);
@@ -268,9 +328,9 @@ public class App {
 			String storageName = splitter1[splitter1.length - 1];
 			storageName = storageName.replace("%20", " ");
 			User user = null;
-			System.out.println("radi remote........");
+			//System.out.println("radi remote........");
 
-			// TODO initRemoteStorage
+			// initRemoteStorage
 			DirectoryRemoteManipulation remoteSt = new DirectoryRemoteManipulation();//Treba da se postavi root
 			FileRemoteManipulation remoteF = new FileRemoteManipulation();//Postavka root-a i zabranjenih ekstenzija
 
@@ -403,7 +463,6 @@ public class App {
 									writer.print("");
 									writer.close();
 								} catch (FileNotFoundException e) {
-									// TODO Auto-generated catch block
 									e.printStackTrace();
 								}
 				} catch (Exception e) {
@@ -437,6 +496,37 @@ public class App {
 				command = sc.nextLine();
 				if (command.equals("exit"))
 					break;
+				if (command.equals("help")) {
+					System.out.println("1 - General Commands:");
+					System.out.println("exit     -     [Exit the program.]");
+					System.out.println("help     -     [Displays all commands.]");
+					
+					System.out.println("2 - User Operations:");
+					System.out.println("mkusr username password priv1 priv2 priv3 priv4     -     [Create User]");
+					System.out.println("rmusr username     -     [Delete User]");
+					System.out.println("lsusr     -     [List All Users]");
+					
+					System.out.println("3 - File Operations:");
+					System.out.println("mkf file_name destination_path     -     [Create File]");
+					System.out.println("rmf file_name     -     [Delete File]");
+					System.out.println("upf file_path destination_path     -     [Upload File]");
+					System.out.println("upmf file1_path file2_path... destination_path archive_name     -     [Upload Multiple Files]");
+					System.out.println("dlf file_path destination_path     -     [Download File]");
+					
+					System.out.println("4 - Directory Operations:");
+					System.out.println("mkdir dir_name destination_path     -     [Create Directory]");
+					System.out.println("mkdir {number} dir_name destination_path     -     [Create {number} Directories");
+					System.out.println("rmdir dir_path     -     [Delete Directory]");
+					System.out.println("updir dir_path destination_path     -     [Upload Directory]");
+					System.out.println("updir-z dir_path destination_path     -     [Upload Zipped Directory");
+					System.out.println("dldir dir_path destination_path     -     [Download Directory]");
+					System.out.println("ls-a dir_path     -     [List All Files]");
+					System.out.println("ls dir_path     -     [List Files]");
+					System.out.println("ls-s dir_path extension     -     [List Files With Given Extension]");
+					System.out.println("ls-d dir_path     -     [List Directories]");
+					
+					continue;
+				}
 				// ako nema spejs a nije lsusr onda je greska
 				if (!command.contains(" ") && !command.equals("lsusr") && !command.equals("ls-a") && !command.equals("ls")) {
 					System.out.println("Wrong input!");
@@ -539,7 +629,7 @@ public class App {
 				case "ls-d":
 					// ls-d path
 					// List Directories (path)
-					String lsdpath = command.substring(command.indexOf(" ") + 1);
+					//String lsdpath = command.substring(command.indexOf(" ") + 1);
 					String lsdparts[] = command.split(" ");
 					if(lsdparts.length == 2)
 						remoteSt.listDirectories(lsdparts[1]);
